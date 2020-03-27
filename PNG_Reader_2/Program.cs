0fad286c2f1fd45f5984c0a172078e7bdbc7747e
Student 241540 <241540@student.pwr.edu.pl>
2020-03-27 17:15:57 +0100

    Add project

---
+++ PNG_Reader_2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PNG_Reader_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
